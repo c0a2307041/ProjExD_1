ffeb48036cb1548a5ac574131b6a4de820829576
shota <c0a2307041@edu.teu.ac.jp>
2024-04-16 17:01:57 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -27,6 +27,7 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
+        screen.blit(bg_img, [-x+6400, 0])
 
         screen.blit(kk_img,[300,200])
         pg.display.update()
