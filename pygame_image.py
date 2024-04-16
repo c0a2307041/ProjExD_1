2edcf344e46417804b2707c831712412debdc800
shota <c0a2307041@edu.teu.ac.jp>
2024-04-16 17:57:47 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -15,15 +15,15 @@ def main():
     img = pg.image.load("fig/3.png")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_rct = kk_rct.get_rect()
+    kk_rct = kk_img.get_rect()
     kk_rct.center = 300 , 200
-    pg.display.update()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-            x = tmr%3200 
+        x = tmr%3200 
         key_lst=pg.key.get_pressed()
+        '''
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0,-1))
         if key_lst[pg.K_DOWN]:
@@ -31,7 +31,10 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            kk_rct.move_ip((+2,0))
+        else:
+            kk_rct.move_ip((-1,0))
+        '''
 
 
 
@@ -39,7 +42,7 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
-        screen.blit(bg_img, [-x+6400, 0])
+    
 
         screen.blit( kk_img, kk_rct )
         pg.display.update()
