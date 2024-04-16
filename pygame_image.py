d0bf183b9108342959f8b0c97871bcf25331f3d6
shota <c0a2307041@edu.teu.ac.jp>
2024-04-16 16:57:09 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -7,24 +7,31 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 600))
     clock  = pg.time.Clock()
-    clock.tick(200)
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img2,True,False)
     img = pg.image.load("fig/3.png")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     pg.display.update()
     tmr = 0
+    x=1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            x = tmr%3200 
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+
         screen.blit(kk_img,[300,200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        clock.tick(200)
 
 
 if __name__ == "__main__":
